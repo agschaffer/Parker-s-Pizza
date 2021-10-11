f84aa717d147906e4a504cc943e933302f55fcee
agschaffer <92327818+agschaffer@users.noreply.github.com>
2021-10-11 15:44:13 -0400

    Add files via upload

---
+++ pizzaFunctions.js
@@ -0,0 +1,11 @@
+/* A set of functions to change the background color*/
+
+// Change background to tomato
+function setTomato() {
+    document.body.style.background = "tomato";
+}
+
+// Change background to white
+function setWheat() {
+    document.body.style.background = "Wheat";
+}
